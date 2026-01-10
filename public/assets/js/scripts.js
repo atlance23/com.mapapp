94973d413dbbce056371b0534191572256fed0d4
Noah C. <caelantandrews@outlook.com>
2026-01-10 10:58:13 -0600

    Update syntax

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -298,9 +298,10 @@ function updateInstruction() {
       padding:16px;
       font-family:Roboto,sans-serif;
       border-radius:12px;
-      box-shadow:0 4px 12px rgba(0,0,0,.15)">
+      box-shadow:0 4px 12px rgba(0,0,0,.15)
       background-color: #000;
       color: #FFF;
+      ">
       <div style="font-size:32px">${turnIcon(step)}</div>
       <strong>${step.instruction}</strong><br>
       <small>${formatDistance(step.distance)}</small>
